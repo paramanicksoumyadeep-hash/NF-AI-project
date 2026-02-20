6b77d311468056d59b52302aef486b8109d7c7ca
paramanicksoumyadeep-hash <paramanicksoumyadeep@gmail.com>
2026-02-20 14:13:56 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,6 @@ df = pd.read_csv("data/raw_data.csv")
 le = LabelEncoder()
 df["material"] = le.fit_transform(df["material"])
 
-# Save encoder
 joblib.dump(le, "models/material_encoder.pkl")
 
 df.to_csv("data/processed_data.csv", index=False)
